29d4a8af7826b523a7783e77e56a3be536fa98fe
ortiza5 <43008712+ortiza5@users.noreply.github.com>
2018-12-02 13:27:25 -0500

    made coords a square

--- DrawSquare.m
+++ DrawSquare.m
@@ -10,18 +10,22 @@ angles = GetDobotAngles(dobot);
 % desired = DobotForwardKinematics(angles) + offset
 offset = [0;0;0] - DobotForwardKinematics(angles)
 
-desired = [2;0;0];
+desired = [4;0;0];
 angles = DobotInverseKinematics(desired-offset,angles);
 SetDobotAngles(dobot,angles,3);
 
-desired = [0;2;0];
+desired = [0;0;0];
 angles = DobotInverseKinematics(desired-offset,angles);
 SetDobotAngles(dobot,angles,3);
 
-desired = [0;-2;0];
+desired = [0;4;0];
 angles = DobotInverseKinematics(desired-offset,angles);
 SetDobotAngles(dobot,angles,3);
 
-desired = [-2;0;0];
+desired = [4;4;0];
+angles = DobotInverseKinematics(desired-offset,angles);
+SetDobotAngles(dobot,angles,3);
+
+desired = [4;0;0];
 angles = DobotInverseKinematics(desired-offset,angles);
 SetDobotAngles(dobot,angles,3);
